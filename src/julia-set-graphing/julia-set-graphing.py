d3e2d4fa53855aa5957833ca21d0dd49cbfdf696
sanjitdp <sanjitd@ucla.edu>
2021-06-28 14:00:04 -0700

    switched to PIL library and rendered pixel by pixel for better output

--- src/julia-set-graphing/julia-set-graphing.py
+++ src/julia-set-graphing/julia-set-graphing.py
@@ -1,29 +1,50 @@
-import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+
+# image dimension constants
+WIDTH = 1000
+HEIGHT = 1000
+
+# complex number bound constants
+RE_START = -2
+RE_END = 2
+IM_START = -2
+IM_END = 2
+
+# precision constants
+MAX_ITERATIONS = 80
+MAX_MAGNITUDE = 2
+
+# function whose Julia set to compute
+FUNCTION = (lambda z: (5 * z - z ** 5) / 4)
 
 
-def in_julia_set(f, value, maximum, n):
-    for i in range(1, n):
-        value = f(value)
-        if abs(value) > maximum:
-            return False
-    return True
+# returns n such that f^n(value) <
+def julia_set(value):
+    n = 0
+    while n < MAX_ITERATIONS and abs(value) < MAX_MAGNITUDE:
+        value = FUNCTION(value)
+        n += 1
+    return n
 
 
-def polynomial(x):
-    return x ** 2 - 1
+# create new PIL image
+img = Image.new("RGB", (WIDTH, HEIGHT), (0, 0, 0))
+draw = ImageDraw.Draw(img)
 
+for x in range(0, WIDTH):
+    for y in range(0, HEIGHT):
+        # normalize and convert (x, y) value into complex number
+        c = complex(RE_START + (x / WIDTH) * (RE_END - RE_START),
+                    IM_START + (y / HEIGHT) * (IM_END - IM_START))
 
-if __name__ == "__main__":
-    re_values = []
-    im_values = []
+        # compute n such that f^n(c) > MAX_MAGNITUDE
+        m = julia_set(c)
 
-    for re_tick in range(-2000, 2000):
-        for im_tick in range(-2000, 2000):
-            z = complex(re_tick / 200, im_tick / 200)
-            if in_julia_set(polynomial, z, 2, 25):
-                re_values.append(z.real)
-                im_values.append(z.imag)
-                print(z)
+        # normalizes the color into an RGB 255-scale
+        color = 255 - int(m * 255 / MAX_ITERATIONS)
 
-    plt.plot(re_values, im_values, marker=".", markersize=1)
-    plt.show()
+        # draws a grayscale pixel with n such that f^n(c) > MAX_MAGNITUDE
+        draw.point([x, y], (color, color, color))
+
+# saves image into julia_set_output.png
+img.save("julia_set_output.png", "PNG")
