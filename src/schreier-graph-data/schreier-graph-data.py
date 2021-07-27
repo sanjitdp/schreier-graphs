fixed undirected graph generation and spectrum calculation - adding directed graph's adjacency matrix and transpose to obtain undirected graph - checked spectrum for expected eigenvalues

src/schreier-graph-data/schreier-graph-data.py
@@ -2,20 +2,19 @@ from itertools import product
 from copy import deepcopy
 import networkx as nx
 from networkx import networkx
-import graphviz
+import graphviz as gv
+import numpy as np
 import os
 
 # constants that determine the graph that will be drawn
-
-PERMUTATION_CYCLES = [[0, 1], [0, 2]]  # what are the permutation cycles?
+PERMUTATION_CYCLES = [[0, 1, 2, 3], [0, 3, 4]]  # what are the permutation cycles?
 DIRECTED = False  # are we studying the directed graph?
 TASK = "ADJ_SPECTRUM"  # what task are we doing?
-LEVEL = 4  # level to which the program should be run
+LEVEL = 2  # level to which the program should be run
 DIRNAME = "../../output/schreier-graph-visualization"  # directory in which to save the output file
 
 
 # function to visualize graph
-
 def visualize(nk):
     # change into the output directory
     try:
@@ -29,25 +28,25 @@ def visualize(nk):
     nx.drawing.nx_pydot.write_dot(nk, "graph")
 
     # render graph and remove unnecessary temporary DOT file
-    graphviz.render('neato', 'png', "graph")
+    gv.render('neato', 'png', "graph")
     os.remove("graph")
 
     return "Graph has been drawn successfully!"
 
 
 # dictionary containing all possible tasks
-
 TASKS_DICTIONARY = {
     "VISUALIZE": visualize,
     "RADIUS": lambda nk: networkx.algorithms.distance_measures.radius(nk),
     "DIAMETER": lambda nk: networkx.algorithms.distance_measures.diameter(nk),
     "PERIPHERY": lambda nk: networkx.algorithms.distance_measures.periphery(nk),
     "ECCENTRICITIES": lambda nk: networkx.algorithms.distance_measures.eccentricity(nk),
+    "ADJACENCY_MATRIX": lambda nk: networkx.linalg.graphmatrix.adjacency_matrix(nk).todense(),
     "ADJ_SPECTRUM": lambda nk: networkx.linalg.spectrum.adjacency_spectrum(nk),
-    "LAP_SPECTRUM": lambda nk: networkx.linalg.spectrum.laplacian_spectrum(nk),
-    "NORM_LAP_SPECTRUM": lambda nk: networkx.linalg.spectrum.normalized_laplacian_spectrum(nk),
-    "HES_SPECTRUM": lambda nk: networkx.linalg.spectrum.bethe_hessian_spectrum(nk),
-    "MOD_SPECTRUM": lambda nk: networkx.linalg.spectrum.modularity_spectrum(nk)
+    "LAP_SPECTRUM": lambda nk: networkx.linalg.spectrum.laplacian_spectrum(nk).tolist(),
+    "NORM_LAP_SPECTRUM": lambda nk: networkx.linalg.spectrum.normalized_laplacian_spectrum(nk).tolist(),
+    "HES_SPECTRUM": lambda nk: networkx.linalg.spectrum.bethe_hessian_spectrum(nk).tolist(),
+    "MOD_SPECTRUM": lambda nk: networkx.linalg.spectrum.modularity_spectrum(nk).tolist()
 }
 
 
@@ -92,7 +91,7 @@ def make_graph(word_length):
         edges.append(cycle_edges)
 
     # create a graph using networkx
-    network = nx.MultiDiGraph() if DIRECTED else nx.MultiGraph()
+    network = nx.MultiDiGraph()
 
     # find edges and vertices using above function
     for node in nodes:
@@ -101,6 +100,11 @@ def make_graph(word_length):
     for cycle in edges:
         for edge in cycle:
             network.add_edge(edge, cycle[edge], name=str(cycle))
+
+    if not DIRECTED:
+        directed_matrix = networkx.to_numpy_matrix(network)
+        undirected_matrix = np.add(directed_matrix, directed_matrix.transpose())
+        return networkx.from_numpy_matrix(undirected_matrix, parallel_edges=True, create_using=nx.MultiGraph)
 
     return network
 